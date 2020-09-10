265bbc8675ddf2a426b0673b18260abce4fa52e3
Александр Семенов <korwin86@gmail.com>
2020-09-10 15:20:03 +0700

    Добавьте файлы проекта.

---
+++ CrmUi/Catalog.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using System.Windows.Forms;
+
+namespace CrmUi
+{
+    public partial class Catalog<T> : Form
+        where T : class
+    {
+        public Catalog(DbSet<T> set)
+        {
+            InitializeComponent();
+            dataGridView.DataSource = set.Local.ToBindingList();
+        }
+    }
+}
